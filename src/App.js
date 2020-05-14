9ca003edb66bcfe52e2cd9d4b736fe949e772eb5
Michael Perez <mperez6473@gmail.com>
2020-05-14 15:33:54 -0500

    MVP is done but i cannot remove an item from the cart>

--- src/App.js
+++ src/App.js
@@ -11,17 +11,41 @@ import { StoreContext, CartContext } from './contexts';
 
 function App() {
     const [products] = useState(data);
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState([
+        {
+            id: 1,
+            title: 'The Art Of War',
+            price: 10.11,
+            image:
+                'https://images-na.ssl-images-amazon.com/images/I/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg',
+        },
+        {
+            id: 2,
+            title: 'Civilization and Its Discontents',
+            price: 14.29,
+            image:
+                'https://images-na.ssl-images-amazon.com/images/I/51Jj12iMZnL._SX331_BO1,204,203,200_.jpg',
+        },
+    ]);
 
     console.log(cart);
     const addItem = (item) => {
         // add the given item to the cart
         setCart([...cart, item]);
     };
-
+    const removeItem = (item) => {
+        console.log(
+            'hhheeeeeyyyy',
+            ...cart.filter((value) => {
+                if (value.id !== item.id) {
+                    console.log('it worked');
+                }
+            })
+        );
+    };
     return (
         <div className="App">
-            <CartContext.Provider value={{ cart }}>
+            <CartContext.Provider value={{ cart, removeItem }}>
                 <Navigation cart={cart} />
 
                 <StoreContext.Provider value={{ products, addItem }}>
@@ -32,7 +56,7 @@ function App() {
                 </StoreContext.Provider>
 
                 <Route path="/cart">
-                    <ShoppingCart cart={cart} />
+                    <ShoppingCart />
                 </Route>
             </CartContext.Provider>
         </div>
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -5,7 +5,7 @@ import Item from './ShoppingCartItem';
 import { CartContext } from '../contexts';
 
 const ShoppingCart = () => {
-    const { cart } = useContext(CartContext);
+    const { cart, removeItem } = useContext(CartContext);
     console.log('cart', cart);
     const getCartTotal = () => {
         return cart
@@ -18,7 +18,7 @@ const ShoppingCart = () => {
     return (
         <div className="shopping-cart">
             {cart.map((item) => (
-                <Item key={item.id} {...item} />
+                <Item key={item.id} {...item} removeItem={removeItem} />
             ))}
 
             <div className="shopping-cart__checkout">
